72b93f4779fd8a9eed8d93bcab82f1aa5e155462
Md. Sohel Rana <sohelrana09@gmail.com>
2015-12-05 20:19:39 +0600

    Additional Shipping Block

---
+++ SR/AdditionalShippingBlock/view/frontend/web/js/view/checkout/shipping/additional-block.js
@@ -0,0 +1,12 @@
+define([
+    'uiComponent'
+
+], function (Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'SR_AdditionalShippingBlock/checkout/shipping/additional-block'
+        }
+    });
+});
