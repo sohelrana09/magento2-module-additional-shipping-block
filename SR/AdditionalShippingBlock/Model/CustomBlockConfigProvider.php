ef5a2165e6fddcbf412ed573a71655b6e472bb73
Md. Sohel Rana <sohelrana09@gmail.com>
2015-12-06 13:44:06 +0600

    display block depends on config

---
+++ SR/AdditionalShippingBlock/Model/CustomBlockConfigProvider.php
@@ -0,0 +1,35 @@
+<?php
+namespace SR\AdditionalShippingBlock\Model;
+
+use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class CustomBlockConfigProvider implements ConfigProviderInterface
+{
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfiguration;
+
+
+    /**
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfiguration
+     * @codeCoverageIgnore
+     */
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfiguration
+    ) {
+        $this->scopeConfiguration = $scopeConfiguration;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfig()
+    {
+        $showHide = [];
+        $enabled = $this->scopeConfiguration->getValue('sr_block_config/general/enabled', ScopeInterface::SCOPE_STORE);
+        $showHide['show_hide_custom_block'] = ($enabled)?true:false;
+        return $showHide;
+    }
+}
--- SR/AdditionalShippingBlock/view/frontend/web/js/view/checkout/shipping/additional-block.js
+++ SR/AdditionalShippingBlock/view/frontend/web/js/view/checkout/shipping/additional-block.js
@@ -1,12 +1,12 @@
 define([
     'uiComponent'
-
 ], function (Component) {
     'use strict';
-
+    var show_hide_custom_blockConfig = window.checkoutConfig.show_hide_custom_block;
     return Component.extend({
         defaults: {
             template: 'SR_AdditionalShippingBlock/checkout/shipping/additional-block'
-        }
+        },
+        canVisibleBlock: show_hide_custom_blockConfig
     });
 });
